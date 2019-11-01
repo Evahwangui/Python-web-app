ac65bfba53067b1a570d5aa4d5844aea361f3723
KariukiAbel <abelmuriithi@gmail.com>
2019-11-01 16:06:54 +0300

    App is running

--- script.py
+++ script.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 def home():
     return "Web content goes here!"
 
+# if __name__ == "__.main__":
+#     app.run(debug = True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
