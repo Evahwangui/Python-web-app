0cb05db6065eb0c5cf7939b60fa810a49909e1f4
KariukiAbel <abelmuriithi@gmail.com>
2019-11-01 16:15:12 +0300

    Returning a string on the web browser

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+# importing the flask class object from the flask library
 from flask import Flask
 
-
+# instanciating the flask class. the  __name__ gets the name of the python script. Python assigns 
+# the name __main__ to the python script
 app = Flask(__name__)
 
 #url to view the website. The slash means the home page
@@ -10,6 +12,7 @@ def home():
 
 # if __name__ == "__.main__":
 #     app.run(debug = True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
